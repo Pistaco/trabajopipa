b6c5f707e2bbea21bc528a21c27c3281f9350bac
Leomax43 <maxpastenny@gmail.com>
2025-04-10 18:53:44 -0400

    mono salta y no se cae
    Copiar y pegar en los otros scenes

--- Scenes/jugador.gd
+++ Scenes/jugador.gd
@@ -17,12 +17,23 @@ var speed := 400
 
 func _physics_process(delta):
 	var inputVel = Input.get_axis("ui_left","ui_right")
+	var saltar = Input.get_action_strength("ui_accept")
 	velocity.x = inputVel * speed
+	
+	if saltar !=0 and is_on_floor():
+		velocity.y =0
+		velocity.y -= saltar * 2000
+
+	
+	if !is_on_floor():
+		velocity.y += 100
+	
 	move_and_slide()
 	if velocity.x !=0:
 		animated_sprite_2d.play("run")
 	else:
 		animated_sprite_2d.play("Iddle")
 	
-	
+	if inputVel !=0:
+		animated_sprite_2d.flip_h = inputVel <0
 	print(inputVel)
