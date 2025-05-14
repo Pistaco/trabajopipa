ca66e7b9914352ff24282814f459174721bea951
Leomax43 <130073563+Leomax43@users.noreply.github.com>
2025-05-14 00:38:58 -0400

    MenuPausaInGame
    un menu de pausa maomeno

---
+++ Scripts/menu_pausa.gd
@@ -0,0 +1,36 @@
+extends Control
+
+func _ready():
+	$AnimationPlayer.play("RESET")
+	
+	
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("MenuInGame")
+	
+	
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("MenuInGame")
+	
+func testEsc():
+	if Input.is_action_just_pressed("esc") and !get_tree().paused:
+		pause()
+	elif Input.is_action_just_pressed("esc") and get_tree().paused:
+		resume()
+
+
+func _on_continuar_pressed() -> void:
+	resume()
+
+func _on_restart_pressed() -> void:
+	resume()
+	get_tree().reload_current_scene()
+
+func _on_menu_principal_pressed() -> void:
+	get_tree().quit()
+	
+
+
+func _process(delta):
+	testEsc()
